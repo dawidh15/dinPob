bc8457e67e563286e58e9ddf915b4577b69e75ea
dawidh15 <dawidh15@gmail.com>
2018-05-24 15:19:54 -0600

    Capitulo interacciones entre poblaciones
    Renombrado de archivos

--- source.R
+++ source.R
@@ -307,3 +307,68 @@ transStochMat <- setRefClass("transStochMat",
       }
     )#methods
 )#class
+
+#----- CAP. Interacciones-----
+# Predador-Presa (Lotka y Volterra)
+
+plotLV <- function(N1 = 100,
+                   N2 = 50,
+                   alpha = 1/65,
+                   beta = -60,
+                   r1 = .49,
+                   r2 = .14,
+                   K1 = 1200,
+                   K2 = 10,
+                   t = 30,
+                   dt = 1 /12) {
+  pop1 <- numeric()
+  pop2 <- numeric()
+  timeLine <- numeric()
+  
+  pop1[1] <- N1
+  pop2[1] <- N2
+  
+  
+  i <- 0
+  timeLine <- i
+  while (i < t) {
+    N1. <- pop1[length(pop1)]
+    N2. <- pop2[length(pop2)]
+    
+    N1. <- N1. + r1 * N1. * ((K1 + beta * N2. - N1.) / K1) * dt
+    N2. <- N2. + r2 * N2. * ((K2 + alpha*N1. - N2.) / K2) * dt
+    
+    pop1 <- append(pop1, N1.)
+    pop2 <- append(pop2, N2.)
+    
+    i <- i + dt
+    timeLine <- append(timeLine,i)
+  }
+  par(oma = c(2,2,1,2))
+  plot(
+    c(0, t),
+    range(c(pop1, pop2)),
+    axes = F,
+    type = "n",
+    xlab = "tiempo",
+    ylab = "Tamaño de población"
+  )
+  lines(timeLine,pop1, lty = 1)
+  axis(side = 1)
+  axis(side = 2)
+  par(new = TRUE)
+  plot(
+    timeLine,
+    pop2,
+    lty = 2,
+    axes = FALSE,
+    xlab = "",
+    ylab = "",
+    type = "l"
+  )
+  axis(side = 4)
+  mtext("Coyote", side = 4, line = 2)
+ legend("right",lty = c(1,2), legend = c("presa","predador"))
+  abline(h = 0, lwd=3)
+  box(which = "plot")
+}
